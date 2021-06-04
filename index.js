39b782e9556679793fb73cfbf1085ff2f3bb0bfa
Hyungji Kim <icthelight@naver.com>
2021-06-04 19:21:01 +0900

    added - hamburger btn on header - display property on header, video&faq

---
+++ index.js
@@ -0,0 +1,23 @@
+document.querySelector(".burger").addEventListener('click', function(){
+    if(document.querySelector('nav').style.display ==='none'){
+        document.querySelector('nav').style.display = 'block';
+    }else{
+        document.querySelector('nav').style.display ='none';
+    }
+});
+
+document.querySelector(".faq-2").addEventListener('click', function(){
+    if(document.querySelector('.faq-2 .answer').style.display ==='none'){
+        document.querySelector('.faq-2 .answer').style.display = 'block';
+    }else{
+        document.querySelector('.faq-2 .answer').style.display ='none';
+    }
+});
+
+document.querySelector(".faq-3").addEventListener('click', function(){
+    if(document.querySelector('.faq-3 .answer').style.display ==='none'){
+        document.querySelector('.faq-3 .answer').style.display = 'block';
+    }else{
+        document.querySelector('.faq-3 .answer').style.display ='none';
+    }
+});
